e18d4d63e88f66c7d23459a0d58d5f5487082e3b
YoCere <jcerezorios@gmail.com>
2025-08-24 22:15:49 -0400

    fix(auth): return 404 on user register attempt

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -8,6 +8,14 @@ class ApiController extends Controller
 {
     public function register (Request $request)
     {
-        
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+        return response()->json([
+            "status"=> true,
+            "message"=> "User registered successfully"
+        ], 201);
     }
 }

2519cd0f4534f9d917d006af24238e70e3b16460
YoCere <jcerezorios@gmail.com>
2025-09-09 00:20:38 -0400

    feat(db): Added factory recibos

--- database/factories/recibosFactory.php
+++ database/factories/recibosFactory.php
@@ -4,6 +4,9 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Monolog\Formatter\LineFormatter;
+use App\Models\cliente;
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
@@ -17,8 +20,16 @@ class recibosFactory extends Factory
      */
     public function definition(): array
     {
+    
         return [
-            //
+            'emitido'=>$this->faker->date(),
+            'cliente_id'=> cliente::all()->random()->id,
+            'user_id'=> cliente::all()->random()->id,
+            'periodo_facturado'=>$this->faker->date(),
+            'monto_total'=>$this->faker->randomFloat(2, 50, 500),
+            'monto_multa'=>$this->faker->randomFloat(2, 0, 100),
+            'created_at'=>$this->faker->date(),
+            'updated_at'=>$this->faker->date()
         ];
     }
 }

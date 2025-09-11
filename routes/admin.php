90597684cdb2f81258a31528c3e45baaedce4a12
YoCere <jcerezorios@gmail.com>
2025-09-11 18:54:26 -0400

    feat(Admin): Add customer controller

--- routes/admin.php
+++ routes/admin.php
@@ -2,11 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\HomeController;
-use App\Http\Controllers\Admin\ClienteController;
+use App\Http\Controllers\Admin\ClientController;
 
 Route::middleware(['auth'])
     ->prefix('admin')
     ->name('admin.')
     ->group(function () {
         Route::get('/', [HomeController::class, 'index'])->name('home');
+        Route::resource('clients', ClientController::class)->names('admin.clients');
     });

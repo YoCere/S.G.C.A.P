321a77240a8703255a48147f780ed1dc1524af1e
YoCere <jcerezorios@gmail.com>
2025-12-11 22:57:12 -0400

    fix(cloud):bug in redis config

--- config/redis.php
+++ config/redis.php
@@ -1,13 +1,14 @@
+<?php
 return [
     'client' => env('REDIS_CLIENT', 'predis'),
     'default' => [
-        'host' => env('REDIS_HOST', 'redis.railway.internal'),
+        'host' => env('REDIS_HOST', '127.0.0.1'),
         'password' => env('REDIS_PASSWORD', null),
         'port' => env('REDIS_PORT', 6379),
         'database' => '0',
     ],
     'cache' => [
-        'host' => env('REDIS_HOST', 'redis.railway.internal'),
+        'host' => env('REDIS_HOST', '127.0.0.1'),
         'password' => env('REDIS_PASSWORD', null),
         'port' => env('REDIS_PORT', 6379),
         'database' => '1',
